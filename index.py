1eb6917d9af6384e80e3e26e9d70e7d9f9b4b23b
odrum428 <oxkeita@gmail.com>
2018-11-17 20:28:04 +0900

    作成したURLをbotに返答させる

--- index.py
+++ index.py
@@ -54,18 +54,18 @@ def lambda_handler(event, content):
         post_message_to_slack(msg, event["event"]["channel"])
 
     elif command == "help":
-        msg = "*概要*\nSlackからIssueを作成できるBotです。定期的に作成するissueをこいつに任せると便利になるよ！\n" \
-        "DynamoDBに必要な情報を設定することで簡単に作成できるようになります\n\n" \
-        "*使い方*\nissue情報の登録: このボットにメンションを飛ばして各種情報を入力してください。issueの中身はスペース区切りで改行されます\n" \
-        "コマンド例 `@このボット setting Issueのタイトル 作成先のowner 作成先のリポジトリ issueの中身`\n" \
+        msg = "*概要*\nSlackからIssueを作成できるBotだよ。定期的に作成するissueをこいつに任せると便利になるよ！\n" \
+        "作成するissueの情報をDynamoDBに登録することで簡単にissueを作成できるよ！\n\n" \
+        "*使い方*\nissue情報の登録: このボットにメンションを飛ばして各種情報を入力してね。issueの中身はスペース区切りに入力することで改行できるよ。\n" \
+        "コマンド例 `@このボット setting Issueのタイトル 作成先のowner 作成先のリポジトリ issueの中身...`\n" \
         "ex. `@このボット setting Sampleisuue odrum428 create_github_issue_from_lambda ##目的 slackからissueを作成する`\n\n" \
         "issue情報の更新 : 基本的には登録の時と同じ。updateコマンドに変えるだけ。\n" \
-        "コマンド例 `@このボット update Issueのタイトル 作成先のowner 作成先のリポジトリ issueの中身`\n" \
+        "コマンド例 `@このボット update Issueのタイトル 作成先のowner 作成先のリポジトリ issueの中身...`\n" \
         "ex. `@このボット update Sampleisuue odrum428 create_github_issue_from_lambda ##目的 slackからissueを作成する`\n\n" \
-        "登録したissue一覧の取得 : get_issue_listをつけると取得できる。\n" \
+        "登録したissue一覧の取得 : get_issue_listとだけ入力すると取得できるよ。\n" \
         "コマンド例 `@このボット get_issue_list`\n" \
         "ex. `@このボット get_issue_list`\n\n" \
-        "issueの作成 : 登録した情報を元にissueを作成します。issueのタイトルとowner情報が必要になります。\n" \
+        "issueの作成 : 登録した情報を元にissueを作成するよ。DynamoDBに登録したissueのタイトルとowner情報が必要だよ。\n" \
         "コマンド例 `@このボット create isuueのタイトル 作成先のowner`\n" \
         "ex. `@このボット create Sampleissue odrum428`"
 
@@ -75,35 +75,35 @@ def lambda_handler(event, content):
     elif command == "create":
         title = slack_text[2]
         owner = slack_text[3]
-        res = read_dynamodb(title, owner)['Items']
-
-        # if not getting():
-        #     msg = "GitHubの情報が登録されてないよ！ownerとRepository情報を登録してね。\n" \
-        #     "このボットに以下の形でメンションを飛ばしてね.\n" \
-        #     "setting REPOSITORY_OWNER REPOSITORY_NAME"
-        #     post_message_to_slack(msg, event["event"]["channel"])
-        #     return
+        res = read_dynamodb(title, owner)['Items'][0]
+        if not res:
+            msg = "Issueの情報が登録されてないよ！作成したいIssueの情報を登録してね。\n" \
+            "*使い方*\nissue情報の登録: このボットにメンションを飛ばして各種情報を入力してね。issueの中身はスペース区切りに入力することで改行できるよ。\n" \
+            "コマンド例 `@このボット setting Issueのタイトル 作成先のowner 作成先のリポジトリ issueの中身...`\n"
+            post_message_to_slack(msg, event["event"]["channel"])
+            return
 
         print(res)
-        msg = "Issueの作成先" + "\n" + "owner:" + res[0]['owner'] + "\n" + "repo:" + res[0]['repo']
+        msg = "Issueの作成先" + "\n" + "owner:" + res['owner'] + "\n" + "repo:" + res['repo']
         post_message_to_slack(msg, event["event"]["channel"])
 
-        url = "https://api.github.com/repos/" + res[0]['owner'] + "/" + res[0]['repo'] + "/issues?access_token=" + token
-        print(url)
+        post_url = "https://api.github.com/repos/" + res['owner'] + "/" + res['repo'] + "/issues?access_token=" + token
 
         create_issue = {
-        "title": res[0]['title'],
-        "body": res[0]['body'],
+        "title": res['title'],
+        "body": res['body'],
         # 'label': [] # お好みでlabelをつけて下さい
         }
 
         params = json.dumps(create_issue)
-        print(params)
-        ret = requests.post(url, params)
-        print(ret)
+        ret = requests.post(post_url, params)
+        ret_json = ret.json()
+
+        print(ret_json)
+        print(ret_json['html_url'])
 
         if ret.status_code == 201:
-            msg = "issueを作成したよ！"
+            msg = "issueを作成したよ！\n作成先のURL : " + ret_json['html_url']
         else:
             msg = "エラーが起こってるよ.ログを確認してみてね。"
 
